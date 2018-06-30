17309140003fe722174b77b6ecf58331f482ffd4
Juan Diaz <juan.diaz@dinocloudconsulting.com>
2018-06-30 19:50:13 -0300

    Empezando con la base del profe

--- tp-dsm/App.js
+++ tp-dsm/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import AlbumList from './src/components/AlbumList';
+import PhotoList from './src/components/PhotoList';
+import { Router, Scene } from 'react-native-router-flux';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+// Create a component
+const App = () => (
+    <Router>
+        <Scene key="root">
+            <Scene key="albumList" component={AlbumList} title="Albums" initial={true} />
+            <Scene key="photoList" component={PhotoList} title="Photos" />
+        </Scene>
+    </Router>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
